f0f6cac7884b634aae06e2e3c5bbe39169d4dc75
Haydn Thurman <haydnthurman@yahoo.com>
2022-04-05 13:48:47 -0600

    Added doxygen comments to mainwindow.cpp.

--- Sprite_Editor/mainwindow.cpp
+++ Sprite_Editor/mainwindow.cpp
@@ -41,9 +41,9 @@ MainWindow::~MainWindow(){
     delete ui;
 }
 
-void MainWindow::ReassignDimensions() {
-    AssignDimensions();
-}
+/**
+ * @brief Assigns the dimensions that were given in the starterform and initializes the form.
+ */
 void MainWindow::AssignDimensions() {
     screen = new DrawScreen(otherwindow.imageSize, this);
     realsize.resize(otherwindow.imageSize, otherwindow.imageSize);
@@ -56,6 +56,9 @@ void MainWindow::AssignDimensions() {
     connect(this, &MainWindow::UpdateRealPlayBackSignal, &realsize, &RealSize::UpdateRealPlayBackSlot);
 }
 
+/**
+ * @brief Pops up color picker window.
+ */
 void MainWindow::on_colorPickerPushButton_clicked() {
     QColorDialogTester color;
     currColor = color.returnColor();
@@ -64,6 +67,9 @@ void MainWindow::on_colorPickerPushButton_clicked() {
     ui->currentColorFrame->setStyleSheet(styleSheet);
 }
 
+/**
+ * @brief Adds a frame to the sprite editor.
+ */
 void MainWindow::on_addFrame_clicked()
 {
     emit(UpdateFrame(screen->image));
@@ -71,14 +77,22 @@ void MainWindow::on_addFrame_clicked()
     emit(AddFrame(screen->image));
 }
 
+/**
+ * @brief Changes the main frame.
+ * @param Image to change the frame to.
+ */
 void MainWindow::SetFrame(QImage img){
     screen->clear();
     screen->image = img;
 }
 
+/**
+ * @brief Updates the layout of the horizontal window containing the frames.
+ * @param Array containing all the frames.
+ * @param Index of the current selected frame.
+ */
 void MainWindow::UpdateLayout(std::vector<QImage> frames, int currentFrame) {
     ClearLayout();
-    qDebug("updating");
     for (int i = 0; i < frames.size(); i++) {
         QImage img = frames[i];
         QLabel *label = new QLabel("Text", this);
@@ -94,6 +108,9 @@ void MainWindow::UpdateLayout(std::vector<QImage> frames, int currentFrame) {
     }
 }
 
+/**
+ * @brief Clears the layout containing the frames.
+ */
 void MainWindow::ClearLayout() {
     if (ui->horizontalLayout == NULL)
         return;
@@ -107,19 +124,27 @@ void MainWindow::ClearLayout() {
     }
 }
 
+/**
+ * @brief Saves the current sprite.
+ */
 void MainWindow::on_actionSave_triggered()
 {
     QString filename = QFileDialog::getSaveFileName(this, "", "", "Sprite Sheet Project (*.ssp)");
     emit SaveFile(filename, this->otherwindow.imageSize);
 }
 
-
+/**
+ * @brief Loads a specific sprite.
+ */
 void MainWindow::on_actionLoad_triggered()
 {
     QString filename = QFileDialog::getOpenFileName(this, "", "", "Sprite Sheet Project (*.ssp)");
     emit OpenFile(filename);
 }
 
+/**
+ * @brief Opens a new sprite editor.
+ */
 void MainWindow::on_actionNew_triggered()
 {
     qDebug() << "entered action new";
@@ -130,79 +155,121 @@ void MainWindow::on_actionNew_triggered()
     newWin->otherwindow.show();
 }
 
+/**
+ * @brief Closes the current sprite editor.
+ */
 void MainWindow::on_actionClose_triggered()
 {
     hide();
 }
 
+/**
+ * @brief Moves current screen to the next frame.
+ */
 void MainWindow::on_nextButton_clicked()
 {
     emit UpdateFrame(screen->image);
     emit NextFrameSignal();
 }
 
-
+/**
+ * @brief Moves current screen to the previous frame.
+ */
 void MainWindow::on_previousButton_clicked()
 {
     emit UpdateFrame(screen->image);
     emit PreviousFrameSignal();
 }
 
+/**
+ * @brief Updates the playback window.
+ * @param Frame to set the playback window image to.
+ */
 void MainWindow::UpdatePlayBack(QImage frame) {
         ui->playBackLabel->setFixedSize(100, 100);
         ui->playBackLabel->setPixmap(QPixmap::fromImage(frame));
         ui->playBackLabel->setScaledContents(true);
 }
 
-
+/**
+ * @brief Changes the frames per second value.
+ * @param Value that the slider is changed to.
+ */
 void MainWindow::on_playSpeedHorizontalSlider_valueChanged(int value)
 {
     UpdateFPS(value);
 }
 
-
+/**
+ * @brief Brush type changes to eraser.
+ */
 void MainWindow::on_eraserRadioButton_clicked()
 {
     screen->brushType = "eraser";
 }
 
-
+/**
+ * @brief Brush trype changes to pencil.
+ */
 void MainWindow::on_pencilRadioButton_clicked()
 {
     screen->brushType = "pencil";
 }
 
-
+/**
+ * @brief Brush type changes to brush.
+ */
 void MainWindow::on_brushRadioButton_clicked()
 {
     screen->brushType = "brush";
 }
 
-
+/**
+ * @brief Deletes the current frame.
+ */
 void MainWindow::on_deleteFrame_clicked()
 {
     emit RemoveFrameSignal();
 }
 
+/**
+ * @brief Updates the drawing frame.
+ * @param Image to update the drawing frame to.
+ */
 void MainWindow::UpdateDrawingFrame(QImage img) {
     emit UpdateFrame(img);
 }
 
+/**
+ * @brief Shows the playback window with the real size.
+ */
 void MainWindow::on_playPushButton_clicked()
 {
     realsize.show();
 }
 
+/**
+ * @brief Updates the real size play back window.
+ * @param Image to change the playback window to.
+ */
 void MainWindow::UpdateRealPlayBack(QImage frame) {
     emit UpdateRealPlayBackSignal(frame, otherwindow.imageSize);
 
 }
+
+/**
+ * @brief Duplicates the selected frame.
+ */
 void MainWindow::on_duplicateButton_clicked()
 {
     emit(UpdateFrame(screen->image));
     emit(AddFrame(screen->image));
 }
 
+/**
+ * @brief Changes screen size when opening a new file.
+ * @param Size to be changed to.
+ */
 void MainWindow::ChangeScreenSize(int size)
 {
     emit(UpdateSize(size));
